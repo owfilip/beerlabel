dc72b663cbbe78598fc4451021601ca4efea0198
Filip Owczarzak <owfilip@gmail.com>
2018-10-31 22:21:40 +0100

    Preview element added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PreviewComponent } from './preview/preview.component';
+import { PreviewElementComponent } from './preview/preview-element.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PreviewComponent,
+    PreviewElementComponent
   ],
   imports: [
     BrowserModule
