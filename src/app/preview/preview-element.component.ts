ed5c481a19ad7b0ce13aa2873efc6a0ca4a8b694
Filip Owczarzak <owfilip@gmail.com>
2018-11-01 20:48:59 +0100

    Data as observable

--- src/app/preview/preview-element.component.ts
+++ src/app/preview/preview-element.component.ts
@@ -1,18 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataService } from '../services/data.service';
 import { LabelDetails } from '../models/label-details.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-preview-element',
   templateUrl: './preview-element.component.html',
   styleUrls: ['./preview-element.component.css']
 })
-export class PreviewElementComponent implements OnInit {
+export class PreviewElementComponent implements OnInit, OnDestroy {
   private labelDetails: LabelDetails;
+  private subscription: Subscription;
 
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
-    this.labelDetails = this.dataService.fetchLabel();
+    this.subscription = this.dataService.fetchLabel().subscribe((data: LabelDetails) => {
+      this.labelDetails = data;
+    },
+    (error) => console.log(error));
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
