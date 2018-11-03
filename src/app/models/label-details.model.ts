16761cd19f2e9c6a288c9a9221ddd6a8b79e6279
Filip Owczarzak <owfilip@gmail.com>
2018-11-03 22:14:53 +0100

    Added missing files

---
+++ src/app/models/label-details.model.ts
@@ -0,0 +1,10 @@
+export class LabelDetails {
+  id: string;
+  title: string;
+  alcohol: string;
+  extract: string;
+  malts: string;
+  hops: string;
+  yeast: string;
+  bottlingDate: string;
+}
---
+++ src/app/services/data.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { LabelDetails } from '../models/label-details.model';
+import { Observable, Subject, BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  private label: LabelDetails = {
+    id: '1',
+    title: 'A Beer Title',
+    alcohol: '5,5',
+    extract: '14,5',
+    malts: 'pilzneński, pale ale, pszeniczny, karmelowy, melanoidynowy',
+    hops: 'sybilla, ahtanum, simcoe, citra, centennial',
+    yeast: 'US-04',
+    bottlingDate: '19 listopada 2018'
+  };
+
+  constructor() { }
+
+  saveLabel(labelDetails: LabelDetails): void {
+    this.label = labelDetails;
+  }
+
+  fetchLabel(): Observable<LabelDetails> {
+    const obs = new BehaviorSubject<LabelDetails>(new LabelDetails());
+    obs.next(this.label);
+    return obs.asObservable();
+  }
+}
